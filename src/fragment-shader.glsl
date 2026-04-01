eb32bec1591336642408c45d2a292a3f382a833f
adrian <nyancatgaming9999@gmail.com>
2026-03-31 17:01:19 -0700

    radiation pattern

--- src/fragment-shader.glsl
+++ src/fragment-shader.glsl
@@ -4,13 +4,15 @@ precision highp float;
 
 #define PI 3.1415926538
 #define WAVE_SPEED 50.0
-#define SOURCE_RADIUS 5.0
+#define SOURCE_RADIUS 3.0
 
 uniform vec2 inResolution;
 uniform float inTime;
 uniform float inFreq;
 uniform float inSeparation;
+uniform float inPointAngle;
 uniform int inSourceCount;
+uniform int inPlotMagnitude;
 
 out vec4 outColor;
 
@@ -19,25 +21,37 @@ void main() {
     // compute some wave properties
     float wl = WAVE_SPEED / inFreq;
     float k = 2.0*PI / wl;
-    float phi = inFreq * 2.0 * PI;
+    float omega = inFreq * 2.0 * PI;
+    float separation = inSeparation * wl;
 
-    // accumulate contribution from sources
-    float waveVal = 0.0;
+    // compute phasor
+    float phasor_real = 0.0, phasor_imag = 0.0;
     float circleAlpha = 0.0;
-    float arrayWidth = inSeparation * float(inSourceCount-1);
+    float phase_advance = cos(inPointAngle) * separation * k;
+
     for(int i = 0; i < inSourceCount; i++) {
 
         vec2 sourcePos = vec2(
-            inResolution.x/2.0 + float(i)*inSeparation - arrayWidth/2.0,
+            inResolution.x/2.0 + separation*(float(i) - float(inSourceCount) / 2.0),
             inResolution.y/2.0
         );
 
         float dist = distance(sourcePos, gl_FragCoord.xy);
-        waveVal += cos(dist*k - inTime*phi) / float(inSourceCount);
         circleAlpha += 1.0 - smoothstep(SOURCE_RADIUS, SOURCE_RADIUS + 1.0, dist);
-    
+        phasor_real += cos(-dist*k + phase_advance*float(i)) / float(inSourceCount);
+        phasor_imag += sin(-dist*k + phase_advance*float(i)) / float(inSourceCount);
+        
     }
 
-    outColor = (1.0 - circleAlpha) * vec4(waveVal, 0, -waveVal, 1) + circleAlpha * vec4(1,1,0,1);
+    vec4 circleColor = vec4(1, 1, 0, 1);
+    if(bool(inPlotMagnitude)) {
+        float magnitude = sqrt(phasor_imag*phasor_imag + phasor_real*phasor_real);
+        outColor = (1.0 - circleAlpha) * vec4(0, magnitude, 0, 1) + circleAlpha * circleColor;
+    } else {
+        float osc_real = cos(omega*inTime);
+        float osc_imag = sin(omega*inTime);
+        float waveVal = (osc_real * phasor_real - osc_imag * phasor_imag);
+        outColor = (1.0 - circleAlpha) * vec4(waveVal, 0, -waveVal, 1) + circleAlpha * circleColor;
+    }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,44 @@
 const mainCanvas = document.getElementById("main-view"),
       gl = mainCanvas.getContext("webgl2");
 
-// warn user if webgl not available
-// TODO
+const radPatternCanvas = document.getElementById("rad-pattern"),
+      ctx = radPatternCanvas.getContext("2d");
+
+const sourceCountSlider = document.getElementById("source-count");
+const separationSlider = document.getElementById("separation");
+const pointingAngleSlider = document.getElementById("pointing-angle");
+const plotMagnitudeButton = document.getElementById("plot-magnitude");
+const plotRadPatternButton = document.getElementById("plot-rad-pattern");
+
+const sourceCountDisplay = document.getElementById("source-count-display");
+const separationDisplay = document.getElementById("separation-display");
+const pointingAngleDisplay = document.getElementById("pointing-angle-display");
+
+if(!gl) {
+    alert("WebGL not available :(");
+}
+
+// add entries to datalist
+const datalist = document.getElementById("separation-steplist");
+for(let i = 0; i <= separationSlider.max; i += 0.25) {
+    const option = document.createElement("option");
+    option.value = i;
+    datalist.append(option);
+}
 
 // phased array parameters
 let waveFrequency = 3;
-let sourceCount = 5;
-let separation = 30;
+let sourceCount = null;
+let separation = null;
+let pointingAngle = null;
 
 // resize canvas
 const resizeCanvas = () => {
     const rect = mainCanvas.getBoundingClientRect();
     mainCanvas.width = rect.width * window.devicePixelRatio;
     mainCanvas.height = rect.height * window.devicePixelRatio;
+    radPatternCanvas.width = mainCanvas.width;
+    radPatternCanvas.height = mainCanvas.height;
 };
 
 // webgl objects
@@ -23,7 +48,9 @@ const glUniforms = {
     time: {name: "inTime"},
     frequency: {name: "inFreq"},
     sourceCount: {name: "inSourceCount"},
-    separation: {name: "inSeparation"}
+    separation: {name: "inSeparation"},
+    plotMagnitude: {name: "inPlotMagnitude"},
+    pointingAngle: {name: "inPointAngle"}
 };
 
 const makeShader = async (url, type) => {
@@ -100,25 +127,94 @@ const renderFields = (time) => {
     gl.uniform1f(glUniforms.frequency.attr, waveFrequency);
     gl.uniform1i(glUniforms.sourceCount.attr, sourceCount);
     gl.uniform1f(glUniforms.separation.attr, separation);
+    gl.uniform1i(glUniforms.plotMagnitude.attr, plotMagnitudeButton.checked);
+    gl.uniform1f(glUniforms.pointingAngle.attr, pointingAngle);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 };
 
-gl.canvas.width = 500;
-gl.canvas.height = 500;
+const handleInput = () => {
+    
+    // read inputs
+    sourceCount = sourceCountSlider.value;
+    separation = separationSlider.value;
+    pointingAngle = pointingAngleSlider.value / 180 * Math.PI;
+
+    // update readouts
+    sourceCountDisplay.textContent = sourceCount;
+    separationDisplay.textContent = `${Number(separation).toFixed(2)}\u03bb`;
+    pointingAngleDisplay.textContent = `${Number(pointingAngleSlider.value).toFixed(2)}\u00b0`;
+
+    if(plotRadPatternButton.checked) {
+        plotRadPattern();
+    } else {
+        ctx.clearRect(0, 0, radPatternCanvas.width, radPatternCanvas.height);
+    }
+    
+};
+
+const plotRadPattern = () => {
+
+    ctx.clearRect(0, 0, radPatternCanvas.width, radPatternCanvas.height);
+
+    const steeringPhase = Math.cos(pointingAngle) * separation * 2 * Math.PI;
+    
+    const points = 1000;
+    const radius = 0.9 * Math.min(mainCanvas.width, mainCanvas.height)/2;
+    ctx.strokeStyle = "#ff9900";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    for(let i = 0; i < points; i++) {
+        
+        const angle = i/points * 2*Math.PI;
+        let phasorReal = 0, phasorImag = 0;
+
+        for(let source = 0; source < sourceCount; source++) {
+            const sourceX = separation*(source - sourceCount / 2);
+            const phase = steeringPhase*source + Math.cos(angle)*sourceX*2*Math.PI;
+            phasorReal += Math.cos(phase) / sourceCount;
+            phasorImag += Math.sin(phase) / sourceCount;
+        }
+
+        const mag = Math.sqrt(phasorReal**2 + phasorImag**2);
+        const x = radPatternCanvas.width / 2 + Math.cos(angle) * mag * radius;
+        const y = radPatternCanvas.height / 2 + Math.sin(angle) * mag * radius;
+       
+        if(i == 0)
+            ctx.moveTo(x, y);
+        else
+            ctx.lineTo(x, y);
+
+    }
+    ctx.closePath();
+    ctx.stroke();
+
+};
 
 let timeStart = Date.now();
 const draw = () => {
+
     if(glReady) {
         const time = (Date.now() - timeStart) / 1000;
         renderFields(time);
     }
+
     requestAnimationFrame(draw);
+
 };
 
 // ensure canvas always is appropriately sized
 resizeCanvas();
-const resizeObserver = new ResizeObserver(resizeCanvas);
+const resizeObserver = new ResizeObserver(() => {
+    requestAnimationFrame(resizeCanvas);
+});
 resizeObserver.observe(mainCanvas);
+
+// handle inputs
+handleInput();
+sourceCountSlider.addEventListener("input", handleInput);
+separationSlider.addEventListener("input", handleInput);
+pointingAngleSlider.addEventListener("input", handleInput);
+plotRadPatternButton.addEventListener("input", handleInput);
 
 setupWebgl();
 draw();
